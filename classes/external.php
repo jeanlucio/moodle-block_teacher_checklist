26e3874e4e039f56eca9c9bea378f81808172dd8
Jean Lúcio <jeanlucio@gmail.com>
2026-04-02 12:19:22 +0000

    Fix PHPUnit test failures and phpcs warnings

--- classes/external.php
+++ classes/external.php
@@ -18,10 +18,10 @@ declare(strict_types=1);
 
 namespace block_teacher_checklist;
 
-use external_api;
-use external_function_parameters;
-use external_value;
-use external_single_structure;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_value;
+use core_external\external_single_structure;
 use context_course;
 use stdClass;
 
